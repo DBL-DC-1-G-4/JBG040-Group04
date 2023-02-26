36de10bfa777e85150aabf0f821acc2767d2c000
Žygimantas Krasauskas <zygimantaskrasauskas@wlan-186080.nbw.tue.nl>
2023-02-26 17:24:22 +0100

    created a function for transforming npy into a pytorch object

--- augment.py
+++ augment.py
@@ -1,13 +1,36 @@
 import torch
 from torchvision import transforms
-from image_dataset import ImageDataset
+#from image_dataset import ImageDataset
 from pathlib import Path
 import numpy as np
 
+def convert_npy_to_torch(npy_file_path):
+    """
+    Converts numpy array into a pytorch object.
+
+    Input:
+    path: path for the numpy file.
+
+    Outputs:
+    tensor object: creates a pytorch object.
+    """
+    
+    npy_file = np.load(npy_file_path)
+    
+    torch_object = torch.from_numpy(npy_file)
+    
+    return torch_object
+
+#xtrain_path = Path("../data/X_train.npy")
+ytrain_path = Path("../data/Y_train.npy")
+
+xtrain_path = r"/Users/zygimantaskrasauskas/Desktop/DS1/data/X_train.npy"
+
+X_train_torch = convert_npy_to_torch(xtrain_path)
+
 torch.manual_seed(689)
 
-
-# Transformation 
+# Transformation
 transform = torch.nn.Sequential(
         transforms.RandomRotation(
             degrees=15
@@ -49,35 +72,32 @@ transform = torch.nn.Sequential(
             p=0.1
             ),
 
-        transforms.ToTensor(),
+        #transforms.ToTensor(),
 
         transforms.Normalize(
             mean=[0.5, 0.5, 0.5],
             std=[0.5, 0.5, 0.5]
             ),
 
-        transforms.resize()  # Dimensions tbd
+        transforms.resize()  # Dimensions 1x128x128
 )
 
-
-xtrain_path = Path("../data/X_train.npy")
-ytrain_path = Path("../data/Y_train.npy")
-
-class NumpyDataset(torch.utils.data.Dataset):
-    def __init__(self, dPath, transform=None):
-        self.data = np.load(dPath)
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, index):
-        x = self.data[index]
-        if self.transform:
-            self.transform(x)
-        return x
-
-
-
-
-scripted_transformed = torch.jit.script(transform)
+# =============================================================================
+# class NumpyDataset(torch.utils.data.Dataset):
+#     def __init__(self, dPath, transform=None):
+#         self.data = np.load(dPath)
+#         self.transform = transform
+# 
+#     def __len__(self):
+#         return len(self.data)
+# 
+#     def __getitem__(self, index):
+#         x = self.data[index]
+#         if self.transform:
+#             self.transform(x)
+#         return x
+# 
+# 
+# scripted_transformed = torch.jit.script(transform)
+# 
+# =============================================================================
