aba2937650ba4af40c5e9add304a564033498088
Žygimantas Krasauskas <zygimantaskrasauskas@Zygimantass-MacBook-Pro-2.local>
2023-03-25 15:05:56 +0100

    working augmentation merging

--- augmentation.py
+++ augmentation.py
@@ -3,9 +3,9 @@ from torchvision import transforms
 from pathlib import Path
 import numpy as np
 import torch
-from torch.utils.data import ConcatDataset
-train_dataset = ImageDataset(Path("/Users/20211922/Documents/DBL1/data/X_train.npy"),
-                             Path("/Users/20211922/Documents/DBL1/data/Y_train.npy"))
+
+train_dataset = ImageDataset(Path("data/X_train.npy"),
+                             Path("data/Y_train.npy"))
 
 train_data = train_dataset.imgs
 train_labels = train_dataset.targets
@@ -21,24 +21,16 @@ augmentation_rules = transforms.Compose([
         translate=(0.1, 0.1),
         scale=(0.9, 1.1),
         shear=10),
-
         transforms.RandomApply([transforms.GaussianBlur(kernel_size=(1, 3))], p=0.1),
         transforms.RandomAdjustSharpness(sharpness_factor=1.3, p=0.1),
         transforms.RandomAutocontrast()]
 )
 
-train_augmented = augmentation_rules(train_data_torch)
-train_augmented = train_augmented.numpy()
+train_augmented = augmentation_rules(train_data_torch).numpy()
 
-np.save("/Users/20211922/Documents/DBL1/data/X_train_augmented.npy", train_augmented)
-np.save("/Users/20211922/Documents/DBL1/data/Y_train_augmented.npy", train_labels)
+train_augmented = np.append(train_data, train_augmented, 0)
+train_augmented_labels = np.append(train_labels, train_labels, 0)
 
-train_dataset_augmented = ImageDataset(Path("/Users/20211922/Documents/DBL1/data/X_train_augmented.npy"), 
-                                       Path("/Users/20211922/Documents/DBL1/data/Y_train_augmented.npy"))
+np.save("data/X_train_augmented.npy", train_augmented)
+np.save("data/Y_train_augmented.npy", train_augmented_labels)
 
-merged_train = ConcatDataset([train_dataset, train_dataset_augmented])
-img, label = merged_train[0]
-
-# print the shape of the image and the label
-print(img.shape)
-print(label)
