6eb52ab026838ec556bc51bdcc760df8987b6316
Žygimantas Krasauskas <zygimantaskrasauskas@Zygimantass-MacBook-Pro-2.local>
2023-03-25 14:09:14 +0100

    updated_code

--- augmentation.py
+++ augmentation.py
@@ -1,43 +1,38 @@
+from image_dataset import ImageDataset
 from torchvision import transforms
-import torch
+from pathlib import Path
 import numpy as np
-import os
-import matplotlib.pyplot as plt
+import torch
 
+train_dataset = ImageDataset(Path("/Users/zygimantaskrasauskas/Desktop/DS1/data/X_train.npy"),
+                             Path("/Users/zygimantaskrasauskas/Desktop/DS1/data/Y_train.npy"))
 
-def get_dirs():
-    parDir = os.path.dirname(os.getcwd())
-    data_path = "data"
-    x_train_path = os.path.join(data_path, "X_train.npy")
-    y_train_path = os.path.join(data_path, "Y_train.npy")
-    x_test_path = os.path.join(data_path, "Y_test.npy")
-    y_test_path = os.path.join(data_path, "Y_test.npy")
-    return parDir, x_train_path, y_train_path, x_test_path, y_test_path
+train_data = train_dataset.imgs
+train_labels = train_dataset.targets
 
+torch.manual_seed(689)
 
-def augment_data():
-    torch.manual_seed(689)
+train_data_torch = torch.from_numpy(train_data).to(dtype=torch.float32)
 
-    parDir, x_train_path, y_train_path, x_test_path, y_test_path = get_dirs()
+augmentation_rules = transforms.Compose([
+    transforms.RandomRotation(degrees=(0,25)),
+    transforms.RandomAffine(
+        degrees=0,
+        translate=(0.1, 0.1),
+        scale=(0.9, 1.1),
+        shear=10),
 
-    x_npy = np.load(x_train_path)
-
-    X_train_torch = torch.from_numpy(x_npy).to(dtype=torch.float32)
-
-    x_tran = transforms.Compose([
-        transforms.RandomRotation(degrees=(0,25)),
-        transforms.RandomErasing(scale=(0.1, 0.1)),
-        transforms.RandomAffine(
-            degrees=0,
-            translate=(0.1, 0.1),
-            scale=(0.9, 1.1),
-            shear=10
-        ),
         transforms.RandomApply([transforms.GaussianBlur(kernel_size=(1, 3))], p=0.1),
         transforms.RandomAdjustSharpness(sharpness_factor=1.3, p=0.1),
-        transforms.RandomAutocontrast()
-        ]
-    )
+        transforms.RandomAutocontrast()]
+)
 
-    x_auged = x_tran(X_train_torch).numpy()
-    np.save("data/augmented/X_train_split.npy", x_auged)
+train_augmented = augmentation_rules(train_data_torch).numpy()
+
+np.save("/Users/zygimantaskrasauskas/Desktop/X_train_augmented.npy", train_augmented)
+np.save("/Users/zygimantaskrasauskas/Desktop/X_test_augmented.npy", train_labels)
+
+train_dataset_augmented = ImageDataset(Path("/Users/zygimantaskrasauskas/Desktop/X_train_augmented.npy"), 
+                                       Path("/Users/zygimantaskrasauskas/Desktop/X_test_augmented.npy"))
+
+merged_train = train_dataset.concatenate(train_dataset_augmented)
